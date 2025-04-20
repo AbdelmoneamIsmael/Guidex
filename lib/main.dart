c033a58eb7c7abe90882ade7089b519e155d6771
AbdelmoneamIsmael <asa.abdo5@gmail.com>
2025-04-20 03:11:54 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -9,6 +10,7 @@ import 'package:guidix/core/const/app_const.dart';
 import 'package:guidix/core/utils/functions/init_hive/init_hive.dart';
 import 'package:guidix/core/utils/functions/initialize_getit/initialize_getit.dart';
 import 'package:guidix/core/utils/notification/notification_handeler.dart';
+import 'package:guidix/firebase_options.dart';
 import 'package:guidix/guidix.dart';
 
 void main() async {
@@ -29,6 +31,10 @@ void main() async {
 }
 
 initializeApplication() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await hiveInitialization();
   initializeGetIt();
   kDeviceToken = await NotificationHelper.init();
